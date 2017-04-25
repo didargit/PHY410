a8196fdf75dd0997dd26322fc62b436efa307888
didargit <didarulb@buffalo.edu>
2017-04-24 23:37:41 -0400

    root_tan.py

---
+++ Lecture11/root_tan.py
@@ -0,0 +1,21 @@
+from root_finding import *
+from math import *
+
+def f ( x ) :
+    return tan(x)
+
+
+print(" Algorithms for root of tan(x)")
+print(" ------------------------------------------------")
+
+print(" 1. Secant Method")
+xi = float ( input(" Enter initial guess xi : ") )
+xf = float ( input(" Enter final guess xf : ") )
+acc = float ( input(" Enter accuracy : ") )
+answer = root_secant(f,xi,xf,acc,1000,True)
+print  str ( answer ) + "\n\n"
+
+print(" 2. Bisection search")
+
+answer = root_bisection(f,xi,xf,acc,1000,True)
+print  str ( answer ) + "\n\n"
